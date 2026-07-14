1c4e9728653df32b84750f6437024de686b66629
yashgautam1205-code <yashgautam1205@gmail.com>
2026-07-14 12:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (80.80%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int k = 1;
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] != nums[i - 1]) {
-                nums[k] = nums[i];
+                nums[k]=nums[i];
                 k++;
             }
         }
