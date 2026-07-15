a5dd5d38f312130e6009d91a2cecfa382fa3393d
yashgautam1205-code <yashgautam1205@gmail.com>
2026-07-15 16:58:46 +0530

    Time: 43 ms (6.24%) | Memory: 28.4 MB (64.19%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string c = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                    c.pop_back();
+            }else{
+                c+=s[i];
+            }
+        }
+        return c;
+    }
+};
