24d93970ea88fea227dc59b7a0cb07f37c7fa088
yashgautam1205-code <yashgautam1205@gmail.com>
2026-07-18 18:33:35 +0530

    Time: 3 ms (13.54%) | Memory: 21.5 MB (6.24%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int itr1=0;
-        int itr2=nums.size()-1;
-        while(itr1<itr2){
-            while(nums[itr1]%2==0 && itr1<itr2){
-                itr1++;
-            }
-            while(nums[itr2]%2!=0 && itr1<itr2){
-                itr2--;
-            }
-            swap(nums[itr1],nums[itr2]);
+        // int itr1=0;
+        // int itr2=nums.size()-1;
+        // while(itr1<itr2){
+        //     while(nums[itr1]%2==0 && itr1<itr2){
+        //         itr1++;
+        //     }
+        //     while(nums[itr2]%2!=0 && itr1<itr2){
+        //         itr2--;
+        //     }
+        //     swap(nums[itr1],nums[itr2]);
+        // }
+        // return nums;
+
+
+        vector<pair<int,int>>temp_result;
+        for(auto elem:nums){
+            temp_result.push_back({elem&1,elem});
+        }
+        sort(temp_result.begin(),temp_result.end());
+        for(int i=0;i<nums.size();i++){
+            nums[i]=temp_result[i].second;
         }
         return nums;
     }
