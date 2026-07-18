0758cd5ed4b7845efbfa8378ed152ee3bc363adc
yashgautam1205-code <yashgautam1205@gmail.com>
2026-07-19 03:23:04 +0530

    Time: 3 ms (5.51%) | Memory: 11.7 MB (28.02%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     std::vector<int> plusOne(std::vector<int>& digits) {
-        for (int i = digits.size() - 1; i >= 0; --i) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
