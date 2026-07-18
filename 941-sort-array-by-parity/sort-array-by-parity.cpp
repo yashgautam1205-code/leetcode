bccc1808a0929bae8dc30e2733918af6f25d0122
yashgautam1205-code <yashgautam1205@gmail.com>
2026-07-18 18:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (54.34%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    static bool comp(const int int1, const int int2){
+        // if(int1&1 == int2&1){
+        //     return false;
+        // }
+        // return int1&1==1;
+        return (int1&1)<(int2&1);
+    }
+
     vector<int> sortArrayByParity(vector<int>& nums) {
         // int itr1=0;
         // int itr2=nums.size()-1;
@@ -15,14 +23,18 @@ public:
         // return nums;
 
 
-        vector<pair<int,int>>temp_result;
-        for(auto elem:nums){
-            temp_result.push_back({elem&1,elem});
-        }
-        sort(temp_result.begin(),temp_result.end());
-        for(int i=0;i<nums.size();i++){
-            nums[i]=temp_result[i].second;
-        }
+        // vector<pair<int,int>>temp_result;
+        // for(auto elem:nums){
+        //     temp_result.push_back({elem&1,elem});
+        // }
+        // sort(temp_result.begin(),temp_result.end());
+        // for(int i=0;i<nums.size();i++){
+        //     nums[i]=temp_result[i].second;
+        // }
+        // return nums;
+
+
+        sort(nums.begin(),nums.end(),comp);
         return nums;
     }
 };
