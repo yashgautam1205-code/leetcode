44bfeb63689a71fc4ffa34c3bedc39d4377a2e7a
yashgautam1205-code <yashgautam1205@gmail.com>
2026-07-19 19:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (35.48%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+=b;
+        while(b!=0)
+        {
+            int temp=a^b;
+            b=(a&b)<<1;
+            a=temp;
+        }
+        return a;
     }
 };
