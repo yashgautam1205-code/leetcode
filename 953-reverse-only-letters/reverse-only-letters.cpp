3a9b3080878fbb36153b3c818134a3db017ddc3c
Yash Gautam <yashgautam1205@gmail.com>
2026-08-02 21:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (82.86%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left=0;
+        int right=s.size()-1;
+
+        while(left<right)
+        {
+            if(!isalpha(s[left]))
+            {
+                left++;
+            }
+            else if(!isalpha(s[right]))
+            {
+                right--;
+            }
+            else
+            {
+                swap(s[left++],s[right--]);
+            }
+        }
+        return s;
+    }
+};
