964862b9e24c7a93ae1a52fd98c4d6391c80e7ab
Yash Gautam <yashgautam1205@gmail.com>
2026-08-13 18:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (83.35%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+
+        vector<int>freq1(256,-1);
+        vector<int>freq2(256,-1);
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(freq1[s[i]]!=freq2[t[i]]){
+                return false;
+            }
+            freq1[s[i]]=i;
+            freq2[t[i]]=i;
+        }
+        return true;
+    }
+};
