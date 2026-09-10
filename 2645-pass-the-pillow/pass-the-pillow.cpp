bf8727c6c03c2236e5257195b0ccf880310928ce
Yash Gautam <yashgautam1205@gmail.com>
2026-09-10 19:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (48.03%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        return n - abs(n - 1 - time % (n * 2 - 2)); 
+        int div = time / (n-1);
+        int rem = time % (n-1);
+
+        if (rem == 0) {
+            if (div % 2 == 0) {
+                return 1;
+            } else {
+                return n;
+            }
+        } 
+        else {
+            if (div % 2 == 0) {
+                return 1 + rem;
+            } else {
+                return n - rem;
+            }
+        }
     }
 };
